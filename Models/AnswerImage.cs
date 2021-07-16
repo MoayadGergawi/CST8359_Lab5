4e70f52cb85d12ba84d457c66ec2cc0152cd09b8
MoayadGergawi <78973992+MoayadGergawi@users.noreply.github.com>
2021-07-16 01:40:33 -0400

    Add files via upload

---
+++ Models/AnswerImage.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab5.Models
+{
+    public class AnswerImage
+    {
+        public int AnswerImageId { 
+            get; 
+            set; 
+        }
+        
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        [DisplayName("File Name")]
+        public String FileName { 
+            get; 
+            set; 
+        }
+        
+        [Required]
+        [Url]
+        public String Url { 
+            get; 
+            set; 
+        }
+        
+        [Required]
+        public Question Question { 
+            get; 
+            set; 
+        }
+    }
+
+    public enum Question
+    {
+        Earth,
+        Computer
+    }
+}
